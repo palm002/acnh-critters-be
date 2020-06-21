46f2ea4f364dfeacbee6a03edd2dada38a3e739e
Mirko <mirkopalancaji@gmail.com>
2020-06-21 20:58:48 +1000

    Add graphql and mongodb

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
+const mongo = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./graphql/schemas');
 
 
-// ROUTES
-app.get('/', (req, res) => {
-   res.send('Home');
+require('dotenv/config');
+
+const url = process.env.DB_CONNECTION
+
+const port = process.env.PORT || 4000;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
 });
 
 
-app.listen(8000); 
+server.applyMiddleware({ app });
+
+// connect to db
+mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to MongoDB')
+);
+
+app.listen({ port }, () =>
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+)
