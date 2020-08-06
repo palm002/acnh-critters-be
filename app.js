dea857e399bf9e3c7e345e60893ffb6b4fa19d8d
Mirko <mirkopalancaji@gmail.com>
2020-08-06 15:55:44 +1000

    Refactor and cleanup app

--- app.js
+++ app.js
@@ -1,55 +1,31 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const { ApolloServer } = require('apollo-server-express');
-const { Fish } = require('./models/Fish')
-const { Insect } = require('./models/Insect')
-const { SeaCreature } = require('./models/SeaCreature')
-const { typeDefs, resolvers } = require ('./graphql/schemas')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const { ApolloServer } = require('apollo-server-express')
 
-require('dotenv/config');
+const { Fish, Insect, SeaCreature } = require('./models')
+const { makeSchema } = require('./graphql/schemas')
+
+require('dotenv/config')
 
 const url = process.env.DB_CONNECTION
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: (req) => ({ ...req, Fish, Insect, SeaCreature }) 
-});
+  schema: makeSchema(),
+  context: (req) => ({ ...req, Fish, Insect, SeaCreature }),
+})
 
-
-server.applyMiddleware({ app });
+server.applyMiddleware({ app })
 
 // connect to db
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log('Connected to MongoDB')
-);
-
-app.listen({ port }, () =>
-    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+  console.log('Connected to MongoDB')
 )
 
-// const { makeExecutableSchema } = require('graphql-tools');
-// const merge = require('lodash/merge');
-// const fishSchema = require('./schemas/fish');
-// const insectSchema = require('./schemas/insect');
-// const schemas = [fishSchema, insectSchema];
-// const merged = schemas.reduce(
-//     (acc, { typeDefs, resolvers }) =>
-//       ({
-//         typeDefs: acc.typeDefs.concat(typeDefs),
-//         resolvers: merge(acc.resolvers, resolvers)
-//       }),
-//     {
-//       typeDefs: [],
-//       resolvers: {}
-//     }
-//   );
-// const executableSchema = makeExecutableSchema(merged);
-// const apolloServer = new ApolloServer({
-//         schema: executableSchema,
-//         context:...
-//         ...
-// });
+app.listen({ port }, () =>
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  )
+)
